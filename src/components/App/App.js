a411067694dc32cdb3a4fe93c6d6eae72d10a2c4
jayc3457 <jay.daniel.canty@gmail.com>
2023-03-13 19:55:30 -0400

    layout the main app components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,6 @@
+import DateContextWrapper from '../DateContextWrapper/DateContextWrapper';
+import DateNavButton from '../DateNavButton/DateNavButton';
+import ContentRenderer from '../ContentRenderer/ContentRenderer';
 import './App.css';
 
 function App() {
@@ -6,6 +9,13 @@ function App() {
       <header className="App-header">
         <h1>Astrological Photo of the Day</h1>
       </header>
+      <DateContextWrapper>
+        <div className="App-content">
+          <DateNavButton isBackward={true} />
+          <ContentRenderer />
+          <DateNavButton />
+        </div>
+      </DateContextWrapper>
     </div>
   );
 }
